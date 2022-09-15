ca2cb66fe685e5635a19a44e78fb7616871a2432
whitedragon077 <112774207+whitedragon077@users.noreply.github.com>
2022-09-15 18:03:41 +0100

    Update and rename Source.c to lub 2 task 1.c

--- lub 2 task 1.c
+++ lub 2 task 1.c
@@ -4,14 +4,14 @@
 int main()
 {
     double a = 0, b = 0, i = 0;
-    printf("Vvedite kol-vo rubley - ");
+    printf("Enter the amount of rubles - ");
     scanf_s("%f", &a);
-    printf("Vvedite kol-vo rubley, kotoroe xotite polychit - ");
+    printf("Enter the amount of rubles you want to receive - ");
     scanf_s("%f", &b);
     for (i = 0; a < b; ++i)
     {
         a += a * 0.04 / 12;
     }
-    printf("%f mesyacev", i);
+    printf("%f months", i);
     return 0;
 }
