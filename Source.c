5dceab8981e1f0e5a4d217f3fb7fc79b893780af
whitedragon077 <112774207+whitedragon077@users.noreply.github.com>
2022-09-22 14:47:29 +0100

    Add files via upload

---
+++ Source.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main()
+{
+	float a, b, c;
+		int i = 0;
+	printf("Put 3 numbers\n");
+	scanf_s("%f %f %f", &a, &b, &c);
+	if ((int)a!= a) {
+	
+	}
+	else 
+		i++;
+	if ((int)b!= b) {
+		
+	}
+	else
+		i++;
+	if ((int)c!= c) {
+		
+	}
+	else
+		i++;
+	printf("integer numbers %d\n", i);
+	return 0;
+}
